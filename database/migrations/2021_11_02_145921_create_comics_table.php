4217b38d16e2ec68cf4baae3af36ce700b530703
spata00 <sararares12@gmail.com>
2021-11-02 17:45:39 +0100

    first commit

---
+++ database/migrations/2021_11_02_145921_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
